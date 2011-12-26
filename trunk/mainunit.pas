059fb04bc7a70ffa16cd4e517f42b33b904d09f6
JJTrapani@gmail.com <JJTrapani@gmail.com@0eab4757-32c3-f591-fe6d-1bcfe5c5ba99>
2011-12-26 06:37:29 +0000

    1)  Removed un-needed additional file from the DataModule. 2)  NOT COMPLETE - Adding data to the TStringGrid from the qryAmana (TQuery).  I am still trying to figure out how to get the query's record count (qryAmana.RecCount doesn't return what I am expecting!).

--- trunk/mainunit.pas
+++ trunk/mainunit.pas
@@ -38,6 +38,11 @@ Type
 
 
     Procedure FormCreate                (             Sender: TObject           );
+    Procedure FormShow                  (             Sender: TObject           );
+    Procedure grdMainHeaderClick        (             Sender: TObject;
+                                                    IsColumn: Boolean;
+                                                       Index: Integer           );
+    Procedure grdMainResize             (             Sender: TObject           );
     Procedure mnuFileExitClick          (             Sender: TObject           );
     Procedure mnuManagementFlavorsClick (             Sender: TObject           );
     Procedure mnuManagementPricesClick  (             Sender: TObject           );
@@ -48,6 +53,12 @@ Type
     Procedure pmuRemoveClick            (             Sender: TObject           );
     Procedure FormClose                 (             Sender: TObject;
                                              Var CloseAction: TCloseAction      );
+
+    Private
+      GridShown : Boolean;
+
+      Procedure ResetColumnHeader       (       NumberOfRows: Integer = 1       );
+      Procedure RefreshGridData;
 
   End;
 
@@ -69,19 +80,118 @@ Uses
   PriceManagementUnit;
 
 
+Const
+  GridColCount = 9;
 
-{ ----------------------------------------------------------------------------- }
-{ ---------------------- FORM-INDUCED PUBLIC METHODS -------------------------- }
-{ ----------------------------------------------------------------------------- }
+  GridColFlavor = 0;
+  GridColDesc   = 1;
+  GridColType   = 2;
+  GridColPrice  = 3;
+  GridColSize   = 4;
+  GridColHeight = 5;
+  GridColDepth  = 6;
+  GridColWidth  = 7;
+  GridColCups   = 8;
+
+
+{ ---------------------------------------------------------------------------- }
+{ ---------------------- FORM-INDUCED PUBLIC METHODS ------------------------- }
+{ ---------------------------------------------------------------------------- }
 
 Procedure TfrmMain.FormCreate                                  (         Sender: TObject               );
 Begin
 
+  GridShown := False;
+
   { Create the datamodule to use throughout the application }
   Application.CreateForm (TdmApp, dmApp);
 
+  { Set up some min. constraints for our main window }
+  frmMain.Constraints.MinWidth  := 500;
+  frmMain.Constraints.MinHeight := 300;
+
 End; { FormCreate Procedure }
-{ ----------------------------------------------------------------------------- }
+{ ---------------------------------------------------------------------------- }
+
+Procedure TfrmMain.FormShow                                    (         Sender: TObject               );
+Var
+  Index  : Integer;
+  Column : TGridColumn;
+
+Begin
+
+  { Do not set the first column as a fixed one...allow us to put in dynamic data into it... }
+  grdMain.FixedCols := 0;
+
+  { Add all the grid columns }
+  For Index := 0 To GridColCount - 1 Do Begin
+
+    { Add a custom column a grid }
+    Column := grdMain.Columns.Add;
+
+    { Make this the X'th column }
+    Column.Index := Index;
+
+  End; { For }
+
+  { Load the grid data }
+  RefreshGridData;
+
+  { Enable resizing of the grid }
+  GridShown := True;
+
+End; { FormShow Procedure }
+{ ---------------------------------------------------------------------------- }
+
+Procedure TfrmMain.grdMainHeaderClick                          (         Sender: TObject;
+                                                                       IsColumn: Boolean;
+                                                                          Index: Integer               );
+Begin
+
+  If (IsColumn) Then Begin
+
+    { Reverse the sorting order }
+    If (grdMain.SortOrder = soDescending)
+      Then grdMain.SortOrder := soAscending
+      Else grdMain.SortOrder := soDescending;
+
+    { Sort the selected column }
+    grdMain.SortColRow (IsColumn, Index);
+
+  End; { If the user clicked on a column }
+
+End; { grdMainHeaderClick }
+{ ---------------------------------------------------------------------------- }
+
+Procedure TfrmMain.grdMainResize                               (         Sender: TObject               );
+Var
+  TotalColumnWidths : Integer;
+  Index             : Integer;
+
+Begin
+
+  If (GridShown) Then Begin
+
+    { Init total column widths }
+    TotalColumnWidths := 0;
+
+    For Index := 0 To grdMain.ColCount - 1 Do Begin
+      TotalColumnWidths := TotalColumnWidths + grdMain.Columns [Index].Width;
+    End; { For }
+
+    For Index := 0 To grdMain.ColCount - 1 Do Begin
+
+      { Use it as a percentage, and round down }
+      grdMain.Columns [Index].Width := Trunc ((grdMain.Columns [Index].Width / TotalColumnWidths) * frmMain.Width);
+
+      { Do not allow the column to get smaller than 30 pixels }
+      If (grdMain.Columns [Index].Width < 30)
+        Then grdMain.Columns [Index].Width := 30;
+
+    End; { For }
+  End; { If grid is shown }
+End; { grdMainResize Procedure }
+{ ---------------------------------------------------------------------------- }
 
 Procedure TfrmMain.mnuFileExitClick                            (         Sender: TObject               );
 Begin
@@ -90,7 +200,7 @@ Begin
   Close;
 
 End; { mnuFileExitClick Procedure }
-{ ----------------------------------------------------------------------------- }
+{ ---------------------------------------------------------------------------- }
 
 Procedure TfrmMain.mnuManagementFlavorsClick                   (         Sender: TObject               );
 Begin
@@ -100,7 +210,7 @@ Begin
   frmFlavorManagement.Free;
 
 End; { mnuManagementFlavorsClick Procedure }
-{ ----------------------------------------------------------------------------- }
+{ ---------------------------------------------------------------------------- }
 
 Procedure TfrmMain.mnuManagementPricesClick                    (         Sender: TObject               );
 Begin
@@ -110,7 +220,7 @@ Begin
   frmPriceManagement.Free;
 
 End; { mnuManagementPricesClick Procedure }
-{ ----------------------------------------------------------------------------- }
+{ ---------------------------------------------------------------------------- }
 
 Procedure TfrmMain.mnuManagementSizeClick                      (         Sender: TObject               );
 Begin
@@ -120,7 +230,7 @@ Begin
   frmSizeManagement.Free;
 
 End; { mnuManagementSizeClick Procedure }
-{ ----------------------------------------------------------------------------- }
+{ ---------------------------------------------------------------------------- }
 
 Procedure TfrmMain.mnuManagementTypesClick                     (         Sender: TObject               );
 Begin
@@ -130,71 +240,27 @@ Begin
   frmTypeManagement.Free;
 
 End; { mnuManagementTypesClick Procedure }
-{ ----------------------------------------------------------------------------- }
+{ ---------------------------------------------------------------------------- }
+
 Procedure TfrmMain.pmuAddClick                                 (         Sender: TObject               );
 Begin
 
-  With dmApp Do Begin
-
-  //  qryAmana.Database := dbAmana;
-{
-    qryAmana.SQL.Text := 'Select Type From PopcornType;';
-
-    qryAmana.Open;
-    While (Not qryAmana.Eof) Do Begin
-      showmessage (qryAmana.Fields [0].AsString);
-      qryAmana.Next;
-    End;
-
-    qryAmana.Close;
- }
-
-  // Check if we have an active connection. If so, let's close it.
-  If (dbAmana.Connected)
-    Then dbAmana.CloseTransactions;
-
-  // Set the connection parameters.
-  ShowMessage ('Opening a connection to server: localhost');
-
-  dbAmana.Open;
-
-  // First lets get a list of available databases.
-  If (dbAmana.Connected) Then Begin
-    ShowMessage ('Retrieving list of available databases.');
-
-    qryAmana.SQL.Text := 'Show Databases;';
-    qryAmana.Open;
-
-    While (Not qryAmana.Eof) Do Begin
-
-      ShowMessage (qryAmana.Fields [0].AsString);
-      qryAmana.Next;
-
-    End;
-
-    qryAmana.Close;
-
-    ShowMessage ('List of databases received!');
-
-  End;
-
-
-  End;
+  ResetColumnHeader;
 
 End; { pmuAddClick Procedure }
-{ ----------------------------------------------------------------------------- }
+{ ---------------------------------------------------------------------------- }
 
 Procedure TfrmMain.pmuEditClick                                (         Sender: TObject               );
 Begin
 
 End; { pmuEditClick Procedure }
-{ ----------------------------------------------------------------------------- }
+{ ---------------------------------------------------------------------------- }
 
 Procedure TfrmMain.pmuRemoveClick                              (         Sender: TObject               );
 Begin
 
 End; { pmuRemoveClick Procedure }
-{ ----------------------------------------------------------------------------- }
+{ ---------------------------------------------------------------------------- }
 
 Procedure TfrmMain.FormClose                                   (         Sender: TObject;
                                                                 Var CloseAction: TCloseAction          );
@@ -204,8 +270,122 @@ Begin
   dmApp.Free;
 
 End; { FormClose Procedure }
-{ ----------------------------------------------------------------------------- }
+{ ---------------------------------------------------------------------------- }
 
+{ ---------------------------------------------------------------------------- }
+{ ---------------------------- PRIVATE METHODS ------------------------------- }
+{ ---------------------------------------------------------------------------- }
+
+Procedure TfrmMain.ResetColumnHeader                           (         NumberOfRows: Integer = 1       );
+Begin
+
+  { Add a new row }
+  grdMain.RowCount := NumberOfRows ;
+
+  { Set up the default column widths }
+  grdMain.Columns [GridColFlavor ].Width := 100;
+  grdMain.Columns [GridColDesc   ].Width := 300;
+  grdMain.Columns [GridColType   ].Width := 100;
+  grdMain.Columns [GridColPrice  ].Width := 80;
+  grdMain.Columns [GridColSize   ].Width := 40;
+  grdMain.Columns [GridColHeight ].Width := 40;
+  grdMain.Columns [GridColDepth  ].Width := 40;
+  grdMain.Columns [GridColWidth  ].Width := 40;
+  grdMain.Columns [GridColCups   ].Width := 50;
+
+  { Set up the default column text }
+  grdMain.Columns [GridColFlavor ].Title.Caption := 'Flavor';
+  grdMain.Columns [GridColDesc   ].Title.Caption := 'Description';
+  grdMain.Columns [GridColType   ].Title.Caption := 'Type';
+  grdMain.Columns [GridColPrice  ].Title.Caption := 'Price';
+  grdMain.Columns [GridColSize   ].Title.Caption := 'Size';
+  grdMain.Columns [GridColHeight ].Title.Caption := 'Height';
+  grdMain.Columns [GridColDepth  ].Title.Caption := 'Depth';
+  grdMain.Columns [GridColWidth  ].Title.Caption := 'Width';
+  grdMain.Columns [GridColCups   ].Title.Caption := 'Cups';
+
+End; { ResetColumnHeader }
+{ ---------------------------------------------------------------------------- }
+
+Procedure TfrmMain.RefreshGridData;
+Var
+  RowIndex    : Integer;
+
+Begin
+
+  { Clear off the grid and reset the row count }
+  grdMain.Clear;
+
+  With dmApp Do Begin
+
+    qryAmana.SQL.Text := 'Select popcornflavors.Flavor, ' +
+                                'popcornflavors.Description, ' +
+                                'popcorntype.Type, ' +
+                                'popcorntypeprices.Price, ' +
+                                'popcornsizes.Size, ' +
+                                'popcornsizes.Height, ' +
+                                'popcornsizes.Depth, ' +
+                                'popcornsizes.Width, ' +
+                                'popcornsizes.Cups ' +
+
+                         'From popcornflavors ' +
+
+                         'Left Outer Join popcorntype On (popcorntype.ID = popcornflavors.Type_Ptr) ' +
+                         'Left Outer Join popcorntypeprices On (popcorntypeprices.Type_Ptr = popcornflavors.Type_Ptr) ' +
+                         'Left Outer Join popcornsizes On (popcornsizes.ID = popcorntypeprices.Size_Ptr) ' +
+                         'Order By popcorntype.Type, popcornsizes.ID, popcornflavors.Flavor ';
+
+    qryAmana.Open;
+
+    { Reset the row count }
+    If (Not qryAmana.Eof)
+      Then begin
+        grdMain.RowCount := 11;
+
+        //Get the number of records we are returning...how?!?
+        ShowMessage (IntToStr (qryAmana.RecordCount));
+
+      end;
+    { Initialize a default RowIndex }
+    RowIndex := 0;
+
+    { Add the header rows }
+    If (grdMain.RowCount > 1)
+      Then ResetColumnHeader (grdMain.RowCount)
+      Else ResetColumnHeader (1);
+
+
+    While (Not qryAmana.Eof) Do Begin
+
+      Application.ProcessMessages;
+
+      { Set the next row }
+      Inc (RowIndex);
+
+if rowindex < 11 then begin
+
+      grdMain.Cells [GridColFlavor, RowIndex] := qryAmana.FieldByName ('Flavor').AsString;
+      grdMain.Cells [GridColDesc, RowIndex]   := qryAmana.FieldByName ('Description').AsString;
+      grdMain.Cells [GridColType, RowIndex]   := qryAmana.FieldByName ('Type').AsString;
+      grdMain.Cells [GridColPrice, RowIndex]  := '$' + FormatFloat ('0.00', qryAmana.FieldByName ('Price').AsFloat);
+      grdMain.Cells [GridColSize, RowIndex]   := qryAmana.FieldByName ('Size').AsString;
+      grdMain.Cells [GridColHeight, RowIndex] := IntToStr (qryAmana.FieldByName ('Height').AsInteger);
+      grdMain.Cells [GridColDepth, RowIndex]  := IntToStr (qryAmana.FieldByName ('Depth').AsInteger);
+      grdMain.Cells [GridColWidth, RowIndex]  := IntToStr (qryAmana.FieldByName ('Width').AsInteger);
+      grdMain.Cells [GridColCups, RowIndex]   := IntToStr (qryAmana.FieldByName ('Cups').AsInteger);
+
+end;
+
+      qryAmana.Next;
+
+    End; { While }
+
+    qryAmana.Close;
+
+  End; { With dmApp }
+
+End; { RefreshGridData Procedure }
+{ ---------------------------------------------------------------------------- }
 
 End.
 
